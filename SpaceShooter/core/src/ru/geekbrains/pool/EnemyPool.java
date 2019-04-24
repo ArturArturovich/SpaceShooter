1aa04a818548b704714faf1ca324317b920bd17d
ArturArturovich <pikartur@yandex.ru>
2019-04-24 20:45:29 +0300

    game update 1.0

---
+++ SpaceShooter/core/src/ru/geekbrains/pool/EnemyPool.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.pool;
+
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.MainShip;
+
+public class EnemyPool extends SpritesPool<Enemy>{
+
+    private BulletPool bulletPool;
+    private MainShip mainShip;
+    private Rect worldBounds;
+
+    public EnemyPool(BulletPool bulletPool, MainShip mainShip, Rect worldBounds) {
+        this.bulletPool = bulletPool;
+        this.mainShip = mainShip;
+        this.worldBounds = worldBounds;
+    }
+
+    @Override
+    protected Enemy newObject() {
+        return new Enemy(bulletPool, mainShip, worldBounds);
+    }}
